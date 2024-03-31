71b2a8b95c9f6d5280ed1cd5b1d59919ad959dfc
sabith <sabithmuhammed136@gmail.com>
2024-03-31 18:07:13 +0530

    hero section complete

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer
+}
