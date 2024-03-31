9deedc906745a55af1439cf38f621c4533caaab3
sabith <sabithmuhammed136@gmail.com>
2024-03-31 23:18:49 +0530

    super quality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { shoe8 } from "../assets/images";
 
 function SuperQuality() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section
+      id="about-us"
+      className="flex justify-between items-centermax-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You
+          <span className="text-coral-red"> Super Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum a
+          excepturi dolores distinctio quod minus sequi adipisci dignissimos
+          consectetur culpa. Voluptatem labore iure quos ut numquam incidunt.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </p>
+        <div className="mt-11">
+          <Button label="View Details"/>
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="Show 8" width={570} height={522} className="object-contain" />
+      </div>
+    </section>
+  );
 }
 
-export default SuperQuality
+export default SuperQuality;
